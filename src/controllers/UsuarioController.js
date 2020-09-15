e237f1c60cf217b936357d5b04387217c889e6cb
huudson-afk <huudson@icloud.com>
2020-09-15 19:51:33 -0300

    Backend com SelectAll e SelectDetail

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -21,3 +21,27 @@ exports.Insert = (req, res, next) => {
         })
         .catch(error => next(error));
 };
+exports.SelectAll = (req, res, next) => {
+    Usuario.findAll()
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            }
+        })
+        .catch(error => next(error));
+}
+ 
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
+
